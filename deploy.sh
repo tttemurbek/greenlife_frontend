58ebc5748b31a226a3e2346b8b35f661c8bf4db5
Temurbek <123800728+tttemurbek@users.noreply.github.com>
2025-05-29 01:33:11 +0900

    fixed deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
-#PRODUCTION
+# Pull latest changes
 git reset --hard
 git checkout master
 git pull origin master
 
-npm i yarn -g
-yarn global add serve
+npm install -g yarn serve
 yarn
 yarn run build
 pm2 start "yarn run start:prod" --name=GREENLIFE-REACT
